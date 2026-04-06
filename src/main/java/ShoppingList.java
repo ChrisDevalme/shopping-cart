edbd9a0c77efb4333a3821a82b511cec9533752e
Chris Devalme <cdevalme@gmail.com>
2026-04-06 15:36:31 -0400

    Created list

--- src/main/java/ShoppingList.java
+++ src/main/java/ShoppingList.java
@@ -1,2 +1,14 @@
 public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("grapes");
+        System.out.println("apples");
+        System.out.println("banana");
+        System.out.println("water");
+        System.out.println("juice");
+        System.out.println("chicken");
+        System.out.println("beef");
+        System.out.println("fish");
+        System.out.println("lettuce");
+        System.out.println("milk");
+    }
 }
